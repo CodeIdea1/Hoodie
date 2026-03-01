f0b57d969315b3329c6757472b79915afc3b696f
sajaahmedjameel <saja@pikyhost.com>
2026-03-01 03:47:54 +0200

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} All rights reserved. Made with ❤️ by Codeidea</p>
+            <p>&copy; {new Date().getFullYear()} All rights reserved. Made with ❤️</p>
         </footer>
     );
 };
